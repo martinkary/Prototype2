1c9e79abbd1b52666ad8a3cf023c925079fc0349
martinkary <danialkary@gmail.com>
2025-09-20 12:28:55 -0700

    Clean: playercontroller script

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-
+    private float xRange = 15.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()   
     {
@@ -17,13 +17,13 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (transform.position.x < -15)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-15, transform.position.y, transform.position.z); 
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); 
         }
-        if (transform.position.x > 15)
+        if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
     }       
 }
