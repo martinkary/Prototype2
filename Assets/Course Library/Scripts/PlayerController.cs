b70ffd907d92ceecd17f764b1a66beea9888fd5b
martinkary <danialkary@gmail.com>
2025-09-28 13:50:59 -0700

    Add: animal collision

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float speed = 15.0f;
     private float xRange = 15.0f;
     public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
